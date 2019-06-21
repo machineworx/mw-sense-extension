07b43a7d179bc61cc60a78391e8881b556bd29a2
Vinod Srinivasan <vinod@machineworksai.com>
2019-06-20 22:12:42 -0400

    Initial Release

---
+++ build/precache-manifest.7d19b6f8eaa2bf66cd13acdd61a196c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0b26208a8465125f32ba",
+    "url": "/static/js/main.0b26208a.chunk.js"
+  },
+  {
+    "revision": "307e916c09215a3b3a37",
+    "url": "/static/js/2.307e916c.chunk.js"
+  },
+  {
+    "revision": "0b26208a8465125f32ba",
+    "url": "/static/css/main.592475fa.chunk.css"
+  },
+  {
+    "revision": "307e916c09215a3b3a37",
+    "url": "/static/css/2.ecf4ef36.chunk.css"
+  },
+  {
+    "revision": "28ee0cef05c25f1df97c9d2ab1ebba47",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.8cde70682fefb36fe74bd1ecf95294b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ddb03b5f416544bb1039",
+    "url": "/static/js/main.ddb03b5f.chunk.js"
+  },
+  {
+    "revision": "307e916c09215a3b3a37",
+    "url": "/static/js/2.307e916c.chunk.js"
+  },
+  {
+    "revision": "ddb03b5f416544bb1039",
+    "url": "/static/css/main.592475fa.chunk.css"
+  },
+  {
+    "revision": "307e916c09215a3b3a37",
+    "url": "/static/css/2.ecf4ef36.chunk.css"
+  },
+  {
+    "revision": "3bb7d514b4e8c16d22c404db57366133",
+    "url": "/index.html"
+  }
+];
